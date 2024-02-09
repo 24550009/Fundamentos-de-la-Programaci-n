e9e9908fa8bbb007e8ad0d6b84f8dcd7dfc90ccf
24550009 <159494911+24550009@users.noreply.github.com>
2024-02-09 16:53:47 -0600

    Add files via upload

---
+++ eva1_2_1dentificadores/EVA1_2_1DENTIFICADORES.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva1_2_1dentificadores;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA1_2_1DENTIFICADORES {
+
+   /**
+    * @param args the command line arguments
+    */
+   public static void main(String[] args) {
+      // TODO code application logic here
+      // VARIABLES:
+      // TIPO DE DATO + NOMBRE DEL IDENTIFICADOR:
+      // short int byte long -- enteros
+      // float doble --numeros con decimal
+      // char-- character-- caracteres
+      // boolean -- verdadero o falso 
+      // string -- cadenas de texto
+      //si no se decla una varaible, no se puede usar
+      int edad; //declaración de una variable
+      String Nombre_Propio;
+      double salario;
+      
+      
+      //ASIGNACION DE VALORES:
+      //IDENTIFICADOR = VALOR;
+      //se coloca el valor 50 dentro de la variable edad
+      edad = 50;
+      Nombre_Propio ="juan perez jolote";
+      salario =  123.56;
+      
+      //COMO LOS USAMSO:
+      System.out.println(Nombre_Propio);
+      System.out.println(edad);
+      System.out.println(salario);
+      
+   }
+   
+}
