a8ee96225c3c01645f86fb57f62ead9db1f7b239
24550009 <159494911+24550009@users.noreply.github.com>
2024-02-15 21:49:59 -0600

    Add files via upload

---
+++ eva1_4_asignacion/EVA1_4_ASIGNACION.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva1_4_asignacion;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA1_4_ASIGNACION {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        
+        int edad;//Declaracion
+        String nombre;
+        String carrera;
+        edad=25;//Asignción--> un solo= SIGNIFICA ASIGNAR
+        nombre = "Miguel Angel Castillo Chavez";
+        carrera= "ingenieria en sistemas computacionales";
+        System.out.println("Tu edad es");
+        System.out.println(edad);
+        System.out.println("Tu nombre es");
+        System.out.println(nombre);
+        System.out.println("Tu carrera es");
+        System.out.println(carrera);
+
+    }
+    
+}
